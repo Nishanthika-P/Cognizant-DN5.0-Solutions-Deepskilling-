1414e9104c808c8dcfde9b4126d7d968d69463a2
Nishanthika <nishanthikapratapp@gmail.com>
2026-07-13 12:28:57 +0530

    Add files via upload

---
+++ Module2_FrontendDev/NISHANTHIKA.P/hands_on_8/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Header from "./components/Header.vue";
+</script>
+
+<template>
+  <Header />
+  <main>
+    <RouterView />
+  </main>
+  <footer class="site-footer">
+    <p>&copy; 2026 Student Portal. Digital Nurture 5.0.</p>
+  </footer>
+</template>
